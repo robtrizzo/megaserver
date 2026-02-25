6bf85f822d398a731bd349a74bc2e4e751e3aeb3
David Kindel <dkindel@gmail.com>
2026-02-24 19:59:52 -0500

    Add basic terraform files

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  shared_credentials_files = [var.creds_file]
+  profile                  = var.creds_profile
+  region                   = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "base-terraform"
+  }
+}
